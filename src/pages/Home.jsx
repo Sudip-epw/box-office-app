4e481e694431418649d1417229580f6ddffd5ac8
Sudip Halder <sudipepw@gmail.com>
2023-09-14 17:44:28 +0530

    isolated search form logic in SearchForm component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,22 @@
 import { useState } from 'react';
 import { searchForShows, searchForPeople } from './../api/tvmaze';
+import SearchForm from '../components/SearchForm';
+
 const Home = () => {
-  const [searchStr, setSearchStr] = useState('');
   const [apiData, setApiData] = useState(null);
   const [apiDataError, setApiDataError] = useState(null);
-  const [searchOption, setSearchOption] = useState('shows');
 
-  const onSearchInputChange = ev => {
-    setSearchStr(ev.target.value);
-  };
 
-  const onRadioChange = ev => {
-    setSearchOption(ev.target.value)
-  }
-
-  const onSearch = async ev => {
-    ev.preventDefault();
+  const onSearch = async ({ q , searchOption }) => {
 
     try {
       setApiDataError(null);
 
     if(searchOption==='shows') {
-      const result = await searchForShows(searchStr);
+      const result = await searchForShows(q);
       setApiData(result);
     }else {
-      const result = await searchForPeople(searchStr);
+      const result = await searchForPeople(q);
       setApiData(result);
     }
       
@@ -32,6 +24,7 @@ const Home = () => {
       setApiDataError(error);
     }
   };
+  
 
   const renderApiData = () => {
     if (apiDataError) {
@@ -39,11 +32,13 @@ const Home = () => {
     }
 
     if (apiData) {
-      return apiData[0].show ? apiData.map(data => (
+      return apiData[0].show 
+      ? apiData.map(data => 
         <div key={data.show.id}>{data.show.name}</div>
-      )) : apiData.map(data => (
+      ) 
+      : apiData.map(data => (
         <div key={data.person.id}>{data.person.name}</div>
-      ))
+      ));
     }
 
     return null;
@@ -51,37 +46,10 @@ const Home = () => {
 
   return (
     <div>
-      <form onSubmit={onSearch}>
-        <input type="text" value={searchStr} onChange={onSearchInputChange} />
-
-        <label>
-          Shows
-          <input 
-            type="radio" 
-            name="search-option" 
-            value="shows" 
-            checked={searchOption === 'shows'} 
-            onChange={onRadioChange} 
-          />
-        </label>
-
-        <label>
-          Actors
-          <input 
-            type="radio" 
-            name="search-option" 
-            value="actors" 
-            checked={searchOption === 'actors'} 
-            onChange={onRadioChange}
-          />
-        </label>
-
-        <button type="submit">Search</button>
-      </form>
+      <SearchForm onSearch={onSearch} />
 
       <div>{renderApiData()}</div>
     </div>
   );
 };
-
 export default Home;
